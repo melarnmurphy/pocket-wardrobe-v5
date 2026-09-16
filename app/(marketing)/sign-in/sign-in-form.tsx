04bad1e9b1dd3df6503b3144a55c65b0cb74318e
Melarn Murphy <melarnmurphy@gmail.com>
2026-09-16 16:18:08 +0930

    avoid autofill on profile fields

--- app/(marketing)/sign-in/sign-in-form.tsx
+++ app/(marketing)/sign-in/sign-in-form.tsx
@@ -103,9 +103,9 @@ export default function SignInForm({
         {isCreate ? (
           <form action={signUpWithPasswordAction} className={styles.authForm} data-1p-ignore="true">
             <input type="hidden" name="next" value={next} />
-            <Field label="your name" name="name" autoComplete="name" data-1p-ignore data-op-ignore placeholder="your name" defaultValue={name} required />
+            <Field label="your name" name="name" autoComplete="off" data-1p-ignore data-op-ignore placeholder="your name" defaultValue={name} required />
             <div className={styles.authTwoUp}>
-              <Field label="date of birth" name="date_of_birth" type="date" autoComplete="bday" data-1p-ignore data-op-ignore defaultValue={dateOfBirth} required />
+              <Field label="date of birth" name="date_of_birth" type="date" autoComplete="off" data-1p-ignore data-op-ignore defaultValue={dateOfBirth} required />
               <LocationField initialValue={location} />
             </div>
             <Field label="email" name="email" type="email" autoComplete="email" placeholder="you@example.com" defaultValue={email} required />
